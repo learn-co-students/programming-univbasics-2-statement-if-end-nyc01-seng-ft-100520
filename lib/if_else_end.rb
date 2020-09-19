027d09f113b2d21d83791a5d0c1a805d2ef04820
Gueshneider Guerrier <gguerrier227@gmail.com>
2020-09-19 00:56:23 +0000

    Done.

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,1 +1,23 @@
-# Write your solution here
+current_time = Time.now
+current_time = current_time.to_i
+
+#OPTION 1
+if current_time % 2 == 0
+  puts "Even!"
+else
+  puts "Odd!"
+end
+
+#OPTION 2
+if Time.now.to_i % 2 == 0
+  puts "Even!"
+else
+  puts "Odd!"
+end
+
+#OPTION 3
+if current_time.even?
+  puts "Even!"
+else
+  puts "Odd!"
+end
